d2ff1a48cdf18e16aff51ce52fe77b9a43e426d6
naufanituharish <harish_naufan@hotmail.com>
2019-09-18 01:44:23 +0700

    Updates

---
+++ precache-manifest.6746a02d365d22a60c7aeacba60f3f6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12acb10203a5de3a9fb50cacddf1e9c5",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "ab89201f5521a62fecf9",
+    "url": "/pokemon/static/css/main.22405dbe.chunk.css"
+  },
+  {
+    "revision": "bd65fe2d0429b02b6bf8",
+    "url": "/pokemon/static/js/2.33df6487.chunk.js"
+  },
+  {
+    "revision": "ab89201f5521a62fecf9",
+    "url": "/pokemon/static/js/main.fd8bc546.chunk.js"
+  },
+  {
+    "revision": "c7b34e90fd5a4298f876",
+    "url": "/pokemon/static/js/runtime~main.9f7d124e.js"
+  },
+  {
+    "revision": "3cfd5e85cd2597eb00d7f5b0139d3b6d",
+    "url": "/pokemon/static/media/logo.3cfd5e85.svg"
+  }
+]);
